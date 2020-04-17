e08551f3859f84cd377b4ed2d594aa7d08df40bf
Alexander Drinkov <adrinkov@gmail.com>
2020-04-17 20:18:01 +0300

    DIP: Trying to solve Problem 6 from Define Classes Exercise

---
+++ CompanyRoster/Employee.cs
@@ -0,0 +1,50 @@
+﻿namespace CompanyRoster
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Employee
+    {
+        private const string DefaultValueForString = "n/a";
+        private const int DefaultValueForInt = -1;
+
+        public Employee(string name, double salary, string position, string department, string email, int age)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+            this.Email = email;
+            this.Age = age;
+        }
+
+        public Employee(string name, double salary, string position, string department, string email)
+            : this(name, salary, position, department, email, DefaultValueForInt)
+        { 
+        }
+
+        public Employee(string name, double salary, string position, string department, int age)
+            : this(name, salary, position, department, DefaultValueForString, age)
+        { 
+        }
+
+        public Employee(string name, double salary, string position, string department)
+            : this(name, salary, position, department, DefaultValueForString, DefaultValueForInt)
+        {
+        }
+
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Salary} {this.Position} {this.Department}";
+        }
+    }
+}
---
+++ CompanyRoster/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace CompanyRoster
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeCount = int.Parse(Console.ReadLine());
+
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 0; i < employeeCount; i++)
+            {
+                string[] employeeArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = employeeArgs[0];
+                double salary = double.Parse(employeeArgs[1]);
+                string position = employeeArgs[2];
+                string deparment = employeeArgs[3];
+
+                Employee employee = null;
+
+                if (employeeArgs.Length == 4)
+                {
+                    employee = new Employee(name, salary, position, deparment);
+                }
+                else if (employeeArgs.Length == 6)
+                {
+                    string email = employeeArgs[4];
+                    int age = int.Parse(employeeArgs[5]);
+
+                    employee = new Employee(name, salary, position, deparment, email, age);
+                }
+                else
+                {
+                    int age = 0;
+
+                    bool isAge = int.TryParse(employeeArgs[4], out age );
+
+                    if (isAge)
+                    {
+                        employee = new Employee(name, salary, position, deparment, age);
+                    }
+                    else
+                    {
+                        employee = new Employee(name, salary, position, deparment, employeeArgs[4]);
+                    }
+                }
+
+                employees.Add(employee);
+            }
+
+            Dictionary<string, List<Employee>> groupsByDepartment = employees.GroupBy(e => e.Department).ToDictionary(g => g.Key, g1 => g1.ToList());
+
+            foreach (var kvp in groupsByDepartment)
+            {
+                var department = kvp.Key;
+                var allEmployees = kvp.Value;
+                
+                foreach (var item in allEmployees)
+                {
+                    Console.WriteLine($"{department} + {item.Name}");
+                }
+            }
+        }
+    }
+}
